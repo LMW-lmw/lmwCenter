73ba81b6eb05c832c5829106ef8d412d058c641f
lmw <2383250163@qq.com>
2020-10-04 15:37:05 +0800

    第四天

--- src/views/personalManagement/shopCar/ShopCar.vue
+++ src/views/personalManagement/shopCar/ShopCar.vue
@@ -72,13 +72,13 @@
           <div class="use-pack">
             <div class="red-pack">红包余额 <span>{{redPack}}</span> 元</div>
             <div class="use-click">
-              <el-switch :width="30" @change = 'change($event)' v-model="value" active-text="使用红包">
+              <el-switch :width="30" @change='change($event)' v-model="value" active-text="使用红包">
               </el-switch>
             </div>
           </div>
           <div class="pack-line"></div>
           <ul class="coupon">
-            <li v-for="item in coupon" @click="useCoupon($event)" class="coupon-info">
+            <li v-for="(item,index) in coupon" @click="useCoupon($event,index)" class="coupon-info" :key="item.id">
               <div class="coupon-left">
                 <div class="coupon-money">
                   <span>¥</span>
@@ -92,7 +92,8 @@
                 </div>
               </div>
               <div class="coupon-right">
-                <div class="coupon-title"><span>{{item.title}}</span> (满<span>{{item.withAmount}}</span>减<span>{{item.usedAmount}}</span>)</div>
+                <div class="coupon-title"><span>{{item.title}}</span> (满<span>{{item.withAmount}}</span>减<span>{{item.usedAmount}}</span>)
+                </div>
                 <div class="coupon-time">
                   <span>有效期至: </span>
                   <span>{{item.endTime}}</span>
@@ -122,11 +123,16 @@
         </div>
       </transition>
     </div>
+<!--    <transition name="el-zoom-in-center">-->
+<!--    <template>-->
+<!--      <el-button class="affirm" type="text" @click="open">111</el-button>-->
+<!--    </template>-->
+<!--    </transition>-->
   </div>
 </template>
 
 <script>
-  import {getShopCars,getRedPacket,getDiscount} from "../../../network/shopCar/shopCar";
+  import {getShopCars, getRedPacket, getDiscount} from "../../../network/shopCar/shopCar";
   import buyStatus from "../../../components/content/buyStatus/buyStatus";
 
   export default {
@@ -145,12 +151,11 @@
         flag: false,
         hint: false,
         coupon: [],
-        couponActive: false
+        employCoupon: false,
+        couponNum: 0
       }
     },
-    computed: {
-
-    },
+    computed: {},
     components: {
       buyStatus
     },
@@ -161,13 +166,13 @@
     },
     mounted() {
       let moveDemo = document.getElementById('pop-up')
-      this.move(moveDemo,300)
+      this.move(moveDemo, 300)
     },
     methods: {
       //请求接口
       getRedPacket(abc) {
         getRedPacket(abc).then(res => {
-          if(res.V!=null){
+          if (res.V != null) {
             this.redPack = res.V
           }
         })
@@ -194,16 +199,23 @@
             this.price2 += row[i].item.disPrice
           }
           this.flag = true
-          this.price = this.totalPrice()
+          this.price = this.totalPrice(this.couponNum)
           document.getElementsByClassName('to-pay')[0].classList.add('color2')
         } else {
           this.price = 0
           this.price2 = 0
+          this.couponNum = 0
           this.flag = false
           this.value = false
           this.show = false
           this.usePack = false
           document.getElementsByClassName('to-pay')[0].classList.remove('color2')
+
+          for (let i = 0; i < document.getElementsByClassName('coupon-info').length; i++) {
+            if (document.getElementsByClassName('coupon-info')[i].classList.contains('clickCoupon')) {
+              document.getElementsByClassName('coupon-info')[i].classList.remove('clickCoupon')
+            }
+          }
         }
       },
       selectAll(selection) {
@@ -222,27 +234,30 @@
         let onClickTime
         if (clickTime != null) {
           onClickTime = parseInt(clickTime)
-        }else{
+        } else {
           onClickTime = 300
         }
         let timeStart, timeEnd, time, posX, posY;
+
         function getTimeNow() {
           let now = new Date();
           return now.getTime()
         }
+
         function mousemove(ev) {
           if (ev == null) ev = window.event; //IE
-          let X = (ev.clientX - posX)>-527?(ev.clientX - posX):-527
-          let Y = (ev.clientY - posY)>-310?(ev.clientY - posY):-310
-          if(X>358){
-            X=358
+          let X = (ev.clientX - posX) > -527 ? (ev.clientX - posX) : -527
+          let Y = (ev.clientY - posY) > -310 ? (ev.clientY - posY) : -310
+          if (X > 358) {
+            X = 358
           }
-          if(Y>131){
-            Y=131
+          if (Y > 131) {
+            Y = 131
           }
           div.style.left = X + "px";
           div.style.top = Y + "px";
         }
+
         div.onmousedown = function (e) {
           timeStart = getTimeNow();
           if (!e) e = window.event; //IE
@@ -261,48 +276,106 @@
           clearInterval(time);
         }
       },
-      close(){
+      close() {
         this.show = false
       },
-      useRedPack(){
-        if(!this.show && this.flag){
+      useRedPack() {
+        if (!this.show && this.flag) {
+
           this.show = true
-        }else{
-          if(!this.show){
+        } else {
+          if (!this.show) {
+            document.getElementsByClassName('please')[0].innerHTML='请选择课程'
             this.hint = true
           }
         }
       },
-      totalPrice() {
-        if(this.usePack){
-          return this.price = (this.price - this.redPack)>0?(this.price - this.redPack):0
-        }else {
-          this.price = this.price2
-          return this.price
+      totalPrice(coupon) {
+        if (coupon == undefined) {
+          coupon = 0
         }
+        this.price = this.price2
+        if (this.usePack) {
+          this.price = (this.price - this.redPack) > 0 ? (this.price - this.redPack) : 0
+        }
+        if (this.employCoupon) {
+          this.price = (this.price - coupon) > 0 ? (this.price - coupon) : 0
+        }
+        if (!this.usePack && !this.employCoupon) {
+          this.price = this.price2
+        }
+        return this.price
       },
-      change($event){
-        if(!this.usePack){
+      change($event) {
+        if (!this.usePack) {
           this.usePack = true
-          this.price = this.totalPrice()
-        }else {
+          this.price = this.totalPrice(this.couponNum)
+        } else {
           this.usePack = false
-          this.price = this.price2
+          this.price = this.totalPrice(this.couponNum)
         }
       },
-      hintClose(){
+      hintClose() {
         this.hint = false
       },
-      useCoupon(e){
+      getSiblings(o) {//获取兄弟元素
+        let a = [];//定义一个数组，用来存o的兄弟元素
+        let p = o.previousSibling;
+        while (p) {//先取o的哥哥们 判断有没有上一个哥哥元素，如果有则往下执行 p表示previousSibling
+          if (p.nodeType === 1) {
+            a.push(p);
+          }
+          p = p.previousSibling//最后把上一个节点赋给p
+        }
+        a.reverse()//把顺序反转一下 这样元素的顺序就是按先后的了
+        let n = o.nextSibling;//再取o的弟弟
+        while (n) {//判断有没有下一个弟弟结点 n是nextSibling的意思
+          if (n.nodeType === 1) {
+            a.push(n);
+          }
+          n = n.nextSibling;
+        }
+        return a//最后按从老大到老小的顺序，把这一组元素返回
+      },
+      useCoupon(e, index) {
         let event = e.currentTarget
-        console.log(11)
-        // for(let i=0; i<document.getElementsByClassName("coupon-info").length; i++){
-        //   console.log('xx')
-        //   document.getElementsByClassName("coupon-info")[i].classList.add("borderNone")
-        // }
-
-        event.classList.add('clickCoupon')
-      }
+        let b = this.getSiblings(event)
+        let couponMoney = this.coupon[index]
+        if (event.classList.contains(('clickCoupon'))) {//判断是否处于使用状态
+          event.classList.remove('clickCoupon')
+          this.employCoupon = false
+          this.totalPrice()
+        } else {
+          if (this.price >= couponMoney.withAmount) {//判断是否可以使用
+            this.employCoupon = true
+            this.couponNum = couponMoney.usedAmount
+            this.totalPrice(this.couponNum)
+            console.log('可以使用')
+            for (let i = 0; i < b.length; i++) {
+              if (b[i].classList.contains('clickCoupon')) {
+                b[i].classList.remove('clickCoupon')
+              }
+            }
+            event.classList.add('clickCoupon')
+          } else {
+            // this.open()
+            this.hint = true
+            document.getElementsByClassName('please')[0].innerHTML='所选课程金额未达到此优惠卷满减条件'
+            console.log('金额不足以使用优惠券');
+          }
+          console.log(this.coupon[index]);
+        }
+      },
+      // open() {
+      //   this.$alert('<span style="font-size: 14px">所选课程金额未达到此优惠卷满减条件</span>', '提示',{
+      //     confirmButtonText: '确定',
+      //     dangerouslyUseHTMLString: true,
+      //     center: true,
+      //     callback: action => {
+      //
+      //     }
+      //   });
+      // }
     }
   }
 </script>
@@ -465,13 +538,13 @@
     transform: rotate(180deg);
   }
 
-  .location{
+  .location {
     position: relative;
     left: 15%;
     bottom: 650px;
   }
 
-  .use-pack{
+  .use-pack {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -479,25 +552,25 @@
     margin: 0 40px;
   }
 
-  .red-pack{
+  .red-pack {
     font-size: 18px;
   }
 
-  .red-pack span{
+  .red-pack span {
     color: rgb(255, 64, 58);
     font-size: 20px;
   }
 
-  .use-click{
+  .use-click {
     font-size: 18px !important;
   }
 
-  .pack-line{
+  .pack-line {
     border-bottom: 1px solid;
     padding: 10px 0px;
   }
 
-  .coupon{
+  .coupon {
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
@@ -508,7 +581,7 @@
     overflow-y: scroll;
   }
 
-  .coupon-info{
+  .coupon-info {
     width: 330px;
     height: 90px;
     background-color: #ffffff;
@@ -521,7 +594,7 @@
     position: relative;
   }
 
-  .coupon-left{
+  .coupon-left {
     width: 111px;
     background: url(../../../assets/img/backgroundImg/coupon.png) no-repeat;
     background-size: 100% 100%;
@@ -532,7 +605,7 @@
     flex-direction: column;
   }
 
-  .used-amount{
+  .used-amount {
     font-size: 34px;
     display: inline-block;
     margin-bottom: 5px;
@@ -542,41 +615,41 @@
     border: 0.01rem dashed #1090ff;
   }
 
-  .borderNone{
+  .borderNone {
     border: none;
   }
 
-  .coupon-right{
+  .coupon-right {
     padding-left: 12px;
     flex: 1;
     display: flex;
     flex-direction: column;
   }
 
-  .coupon-right{
+  .coupon-right {
 
   }
 
-  .coupon-title{
+  .coupon-title {
     font-size: 14px;
     color: #333333;
     margin-top: 12px;
   }
 
-  .coupon-time{
+  .coupon-time {
     font-size: 12px;
     color: #999999;
     margin-top: 10px;
   }
 
-  .coupon-line{
+  .coupon-line {
     background-color: #e4e4e4;
     width: 100%;
     height: 1px;
     margin-top: 8px;
   }
 
-  .coupon-scope{
+  .coupon-scope {
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -585,21 +658,21 @@
     margin-top: 8px;
   }
 
-  #bg-box{
+  #bg-box {
     width: 100%;
     height: 100%;
     position: fixed;
     left: 0;
     top: 0;
-    background-color: rgba(0, 0, 0, .2);
+    background-color: rgba(0, 0, 0, .5);
     z-index: 1000000000;
   }
 
-  #hint{
+  #hint {
     position: fixed;
-    top: 30%;
+    top: 40%;
     left: 40%;
-    width: 300px;
+    width: 400px;
     height: 180px;
     background-color: rgb(255, 255, 255);
     border-radius: 6px;
@@ -608,20 +681,21 @@
     box-sizing: border-box;
   }
 
-  .hint-top{
+  .hint-top {
     display: flex;
     justify-content: space-between;
     align-items: center;
     font-size: 18px;
   }
-  .please{
+
+  .please {
     font-size: 18px;
     color: #666;
     line-height: 70px;
     text-align: center;
   }
 
-  .que-ren{
+  .que-ren {
     transition: .6s;
     width: 40px;
     height: 18px;
@@ -634,9 +708,22 @@
     margin: 16px auto 0px auto;
     cursor: pointer;
   }
-  .que-ren:hover{
+
+  .que-ren:hover {
     box-shadow: 2px 2px 9px #6d6d6d;
     margin-top: 10px;
     color: rgb(255, 254, 205);
   }
+</style>
+
+<style>
+  .el-button--mini, .el-button--small{
+    transition: 0.6s;
+    font-size: 16px !important;
+    /*background-image: linear-gradient(0deg, #ff403a 0%, #ff6949 100%);*/
+  }
+  .el-message-box__title{
+    font-size: 18px !important;
+  }
+
 </style>
